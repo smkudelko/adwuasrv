139dc60a3feca2ee2a266eec7848b404fb9f1c52
Danijel J <admin@danijelj.com>
2013-08-06 12:46:02 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Advanced WUAUSERV Fix 2.4
+=========================
+
+Windows application to fix majority of Windows Update problems. This is the sou$
+
+**ICON LICENCE:** CC-BY-NC Teekatas Suwannakrua
---
+++ adwuasrv.bat
@@ -0,0 +1,65 @@
+::Copyright (c) 2013, Danijel James
+::All rights reserved.
+::
+::Redistribution and use in source and binary forms, with or without
+::modification, are permitted provided that the following conditions are met:
+::
+::1. Redistributions of source code must retain the above copyright notice, this
+::   list of conditions and the following disclaimer.
+::2. Redistributions in binary form must reproduce the above copyright notice,
+::   this list of conditions and the following disclaimer in the documentation
+::   and/or other materials provided with the distribution.
+
+net stop bits
+net stop wuauserv
+Del "%ALLUSERSPROFILE%\Application Data\Microsoft\Network\Downloader\qmgr*.dat"
+sc.exe sdset bits D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;AU)(A;;CCLCSWRPWPDTLOCRRC;;;PU)
+sc.exe sdset wuauserv D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;AU)(A;;CCLCSWRPWPDTLOCRRC;;;PU)
+rmdir /q /s SoftwareDistribution
+regsvr32.exe /s atl.dll
+regsvr32.exe /s urlmon.dll
+regsvr32.exe /s mshtml.dll
+regsvr32.exe /s shdocvw.dll
+regsvr32.exe /s browseui.dll
+regsvr32.exe /s jscript.dll
+regsvr32.exe /s vbscript.dll
+regsvr32.exe /s scrrun.dll
+regsvr32.exe /s msxml.dll
+regsvr32.exe /s msxml3.dll
+regsvr32.exe /s msxml6.dll
+regsvr32.exe /s actxprxy.dll
+regsvr32.exe /s softpub.dll
+regsvr32.exe /s wintrust.dll
+regsvr32.exe /s dssenh.dll
+regsvr32.exe /s rsaenh.dll
+regsvr32.exe /s gpkcsp.dll
+regsvr32.exe /s sccbase.dll
+regsvr32.exe /s slbcsp.dll
+regsvr32.exe /s cryptdlg.dll
+regsvr32.exe /s oleaut32.dll
+regsvr32.exe /s ole32.dll
+regsvr32.exe /s shell32.dll
+regsvr32.exe /s initpki.dll
+regsvr32.exe /s wuapi.dll
+regsvr32.exe /s wuaueng.dll
+regsvr32.exe /s wuaueng1.dll
+regsvr32.exe /s wucltui.dll
+regsvr32.exe /s wups.dll
+regsvr32.exe /s wups2.dll
+regsvr32.exe /s wuweb.dll
+regsvr32.exe /s qmgr.dll
+regsvr32.exe /s qmgrprxy.dll
+regsvr32.exe /s wucltux.dll
+regsvr32.exe /s muweb.dll
+regsvr32.exe /s wuwebv.dll
+regsvr32 /s wudriver.dll
+netsh winsock reset
+net start bits
+net start wuauserv
+bitsadmin.exe /reset /allusers
+shutdown -r -t 00
+
+
+
+
+
